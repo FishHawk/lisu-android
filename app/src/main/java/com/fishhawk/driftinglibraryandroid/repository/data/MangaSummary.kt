24435368bb6053376e9624e51d1dbaf0388f928d
FishHawk <whang177@gmail.com>
2020-05-10 16:20:04 +0800

    Make MangaSummary praceable

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/repository/data/MangaSummary.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/repository/data/MangaSummary.kt
@@ -1,7 +1,11 @@
 package com.fishhawk.driftinglibraryandroid.repository.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class MangaSummary(
     val id: String,
     val title: String,
     var thumb: String
-)
+) : Parcelable
