a1edf6f2c0103ad09fa1af8eb08f7e76abc8f2ba
FishHawk <whang177@gmail.com>
2020-06-21 14:03:37 +0800

    Clean code

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/explore/ExploreFragment.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/explore/ExploreFragment.kt
@@ -2,19 +2,13 @@ package com.fishhawk.driftinglibraryandroid.explore
 
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.fishhawk.driftinglibraryandroid.MainApplication
 import com.fishhawk.driftinglibraryandroid.R
-import com.fishhawk.driftinglibraryandroid.base.MangaListAdapter
 import com.fishhawk.driftinglibraryandroid.databinding.ExploreFragmentBinding
 import com.fishhawk.driftinglibraryandroid.repository.Result
-import com.fishhawk.driftinglibraryandroid.util.SpacingItemDecoration
-import com.hippo.refreshlayout.RefreshLayout
 
 class ExploreFragment : Fragment() {
     private val viewModel: ExploreViewModel by viewModels {
