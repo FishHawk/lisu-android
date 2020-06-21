0c5c776dbd32cb0c58bc2349a538fd97ac4c5468
FishHawk <whang177@gmail.com>
2020-06-21 14:14:55 +0800

    Refactor LibraryViewmodel to inherit MangaListViewModel

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/library/LibraryViewModel.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/library/LibraryViewModel.kt
@@ -1,13 +1,10 @@
 package com.fishhawk.driftinglibraryandroid.library
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.fishhawk.driftinglibraryandroid.base.MangaListViewModel
 import com.fishhawk.driftinglibraryandroid.repository.RemoteLibraryRepository
 import com.fishhawk.driftinglibraryandroid.repository.Result
-import com.fishhawk.driftinglibraryandroid.repository.data.MangaSummary
-import com.fishhawk.driftinglibraryandroid.util.Event
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -17,20 +14,13 @@ class EmptyListException : Exception()
 
 class LibraryViewModel(
     private val remoteLibraryRepository: RemoteLibraryRepository
-) : ViewModel() {
+) : MangaListViewModel() {
     private var address = remoteLibraryRepository.url
     var filter: String = ""
 
-
-    private val _mangaList: MutableLiveData<Result<List<MangaSummary>>> = MutableLiveData()
-    val mangaList: LiveData<Result<List<MangaSummary>>> = _mangaList
-
     fun reload(filter: String) {
         this.filter = filter
-        _mangaList.value = Result.Loading
-        GlobalScope.launch(Dispatchers.Main) {
-            _mangaList.value = remoteLibraryRepository.getMangaList("", filter)
-        }
+        load()
     }
 
     fun reloadIfNeed(filter: String) {
@@ -39,42 +29,29 @@ class LibraryViewModel(
         address = remoteLibraryRepository.url
     }
 
-
-    private val _refreshFinish: MutableLiveData<Event<Throwable?>> = MutableLiveData()
-    val refreshFinish: LiveData<Event<Throwable?>> = _refreshFinish
-
-    fun refresh() {
+    override fun load() {
+        setLoading()
         GlobalScope.launch(Dispatchers.Main) {
-            when (val result = remoteLibraryRepository.getMangaList("", filter)) {
-                is Result.Success -> {
-                    _mangaList.value = result
-                    if (result.data.isEmpty()) _refreshFinish.value = Event(EmptyListException())
-                    else _refreshFinish.value = Event(null)
-                }
-                is Result.Error -> _refreshFinish.value = Event(result.exception)
-            }
+            val result = remoteLibraryRepository.getMangaList("", filter)
+            processLoadResult(result)
         }
     }
 
+    override fun refresh() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val result = remoteLibraryRepository.getMangaList("", filter)
+            processRefreshResult(result)
+        }
+    }
 
-    private val _fetchMoreFinish: MutableLiveData<Event<Throwable?>> = MutableLiveData()
-    val fetchMoreFinish: LiveData<Event<Throwable?>> = _fetchMoreFinish
-
-    fun fetchMore() {
-        val lastId = (_mangaList.value as Result.Success).data.let {
+    override fun fetchMore() {
+        val lastId = (mangaList.value as Result.Success).data.let {
             if (it.isEmpty()) "" else it.last().id
         }
 
         GlobalScope.launch(Dispatchers.Main) {
-            when (val result = remoteLibraryRepository.getMangaList(lastId, filter)) {
-                is Result.Success -> {
-                    (_mangaList.value as Result.Success).data.plus(result.data)
-                    _mangaList.value = _mangaList.value
-                    if (result.data.isEmpty()) _fetchMoreFinish.value = Event(EmptyListException())
-                    else _fetchMoreFinish.value = Event(null)
-                }
-                is Result.Error -> _fetchMoreFinish.value = Event(result.exception)
-            }
+            val result = remoteLibraryRepository.getMangaList(lastId, filter)
+            processFetchMoreResult(result)
         }
     }
 }
