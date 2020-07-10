44be33628270b22093e21ef63385158c83c65649
FishHawk <whang177@gmail.com>
2020-07-10 15:35:49 +0800

    Clean code

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/MainActivity.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.fishhawk.driftinglibraryandroid
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
