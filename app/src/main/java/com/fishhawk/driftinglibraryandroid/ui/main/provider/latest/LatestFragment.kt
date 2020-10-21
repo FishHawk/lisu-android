fae4b68e3bc4a13d8ac60221523a478a8cba009c
FishHawk <whang177@gmail.com>
2020-10-21 22:09:02 +0800

    Add workaround of wrong tag margin

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/main/provider/latest/LatestFragment.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/main/provider/latest/LatestFragment.kt
@@ -1,11 +1,26 @@
 package com.fishhawk.driftinglibraryandroid.ui.main.provider.latest
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.fishhawk.driftinglibraryandroid.repository.remote.model.OptionModels
 import com.fishhawk.driftinglibraryandroid.ui.main.provider.base.ProviderBaseFragment
 
 class LatestFragment : ProviderBaseFragment() {
     override val viewModel: LatestViewModel by viewModels { getViewModelFactory() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)?.apply {
+            setPadding(0, 100, 0, 0)
+        }
+
+    }
 
     override fun getOptionModel(optionModels: OptionModels): Map<String, List<String>> {
         return optionModels.latest
