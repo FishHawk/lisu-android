0ef1fe3e44d6e6105f2931c2b4c47fe5374ad8db
FishHawk <whang177@gmail.com>
2020-10-21 20:53:39 +0800

    Use TabLayout in provider activity

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/provider/ProviderActivity.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/provider/ProviderActivity.kt
@@ -3,11 +3,9 @@ package com.fishhawk.driftinglibraryandroid.ui.provider
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import com.fishhawk.driftinglibraryandroid.R
 import com.fishhawk.driftinglibraryandroid.databinding.ActivityProviderBinding
 import com.fishhawk.driftinglibraryandroid.setting.SettingsHelper
 import com.fishhawk.driftinglibraryandroid.ui.extension.setupTheme
-import com.fishhawk.driftinglibraryandroid.ui.extension.setupWithNavControllerT
 
 class ProviderActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProviderBinding
@@ -24,40 +22,21 @@ class ProviderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         keywords = intent.extras?.getString("keywords")
+        title = intent.extras!!.getString("providerName")!!
 
         binding = ActivityProviderBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
-        title = intent.extras!!.getString("providerName")!!
-        if (savedInstanceState == null) setupBottomNavigationBar()
+
+        val sectionsPagerAdapter = ProviderPagerAdapter(this, supportFragmentManager)
+        binding.viewPager.adapter = sectionsPagerAdapter
+        binding.viewPager.offscreenPageLimit = 3
+        binding.tabs.setupWithViewPager(binding.viewPager)
 
         SettingsHelper.secureMode.observe(this) {
             val flag = WindowManager.LayoutParams.FLAG_SECURE
             if (it) window.addFlags(flag)
             else window.clearFlags(flag)
         }
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        setupBottomNavigationBar()
-    }
-
-    private fun setupBottomNavigationBar() {
-        val bottomNavigationView = binding.navView
-
-        val navGraphIds = listOf(
-            R.navigation.popular,
-            R.navigation.latest,
-            R.navigation.category
-        )
-
-        bottomNavigationView.setupWithNavControllerT(
-            navGraphIds = navGraphIds,
-            fragmentManager = supportFragmentManager,
-            containerId = binding.navHostContainer.id,
-            intent = intent,
-            hasHome = false
-        )
     }
 }
---
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/provider/ProviderPagerAdapter.kt
@@ -0,0 +1,43 @@
+package com.fishhawk.driftinglibraryandroid.ui.provider
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.fishhawk.driftinglibraryandroid.R
+import com.fishhawk.driftinglibraryandroid.ui.provider.category.CategoryFragment
+import com.fishhawk.driftinglibraryandroid.ui.provider.latest.LatestFragment
+import com.fishhawk.driftinglibraryandroid.ui.provider.popular.PopularFragment
+import java.lang.IllegalArgumentException
+
+class ProviderPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    private val popularFragment by lazy { PopularFragment() }
+    private val latestFragment by lazy { LatestFragment() }
+    private val categoryFragment by lazy { CategoryFragment() }
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> popularFragment
+            1 -> latestFragment
+            2 -> categoryFragment
+            else -> throw IllegalArgumentException("impossible position")
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return TAB_TITLES.size
+    }
+
+    companion object {
+        private val TAB_TITLES = arrayOf(
+            R.string.label_popular,
+            R.string.label_latest,
+            R.string.label_category
+        )
+    }
+}
