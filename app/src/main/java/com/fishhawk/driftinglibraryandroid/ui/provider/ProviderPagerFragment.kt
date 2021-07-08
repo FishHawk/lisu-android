498f7a0d68edd21956ea39434e8863f6ea3ea855
FishHawk <whang177@gmail.com>
2021-07-08 19:55:54 +0800

    Refactor provider pager fragment with compose

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/provider/ProviderPagerFragment.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/provider/ProviderPagerFragment.kt
@@ -1,107 +1,221 @@
 package com.fishhawk.driftinglibraryandroid.ui.provider
 
 import android.os.Bundle
-import android.view.*
-import androidx.appcompat.widget.SearchView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
 import androidx.navigation.fragment.findNavController
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.fishhawk.driftinglibraryandroid.R
-import com.fishhawk.driftinglibraryandroid.data.preference.GlobalPreference
 import com.fishhawk.driftinglibraryandroid.data.preference.ProviderBrowseHistory
-import com.fishhawk.driftinglibraryandroid.databinding.ProviderPagerFragmentBinding
+import com.fishhawk.driftinglibraryandroid.data.remote.model.MangaOutline
+import com.fishhawk.driftinglibraryandroid.data.remote.model.OptionModel
 import com.fishhawk.driftinglibraryandroid.ui.MainViewModelFactory
-import com.fishhawk.driftinglibraryandroid.util.setNext
+import com.fishhawk.driftinglibraryandroid.ui.base.MangaDisplayModeButton
+import com.fishhawk.driftinglibraryandroid.ui.base.RefreshableMangaList
+import com.fishhawk.driftinglibraryandroid.ui.base.navToReaderActivity
+import com.fishhawk.driftinglibraryandroid.ui.theme.ApplicationTheme
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.rememberInsetsPaddingValues
+import com.google.accompanist.insets.ui.TopAppBar
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
 
+@OptIn(ExperimentalPagerApi::class)
 class ProviderPagerFragment : Fragment() {
-    private lateinit var binding: ProviderPagerFragmentBinding
     private val viewModel: ProviderViewModel by viewModels {
         MainViewModelFactory(this)
     }
 
-    private lateinit var providerBrowseHistory: ProviderBrowseHistory
+    private val providerBrowseHistory: ProviderBrowseHistory by lazy {
+        ProviderBrowseHistory(requireContext())
+    }
+
+    private val actionAdapter = object : ProviderActionSheet.Listener {
+        override fun onReadClick(outline: MangaOutline, provider: String) {
+            navToReaderActivity(outline.id, viewModel.provider.id, 0, 0, 0)
+        }
+
+        override fun onLibraryAddClick(outline: MangaOutline, provider: String) {
+            viewModel.addToLibrary(outline.id, outline.title)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = ProviderPagerFragmentBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = ComposeView(requireContext())
+        view.setContent {
+            ApplicationTheme {
+                ProvideWindowInsets {
+                    val pagerState = rememberPagerState(
+                        pageCount = 3,
+                        initialPage = providerBrowseHistory
+                            .getPageHistory(viewModel.provider.id)
+                            .coerceIn(0, 2)
+                    )
+                    providerBrowseHistory.setPageHistory(
+                        viewModel.provider.id,
+                        pagerState.currentPage
+                    )
+                    Scaffold(
+                        topBar = { ToolBar(pagerState) },
+                        content = { Content(pagerState) }
+                    )
+                }
+            }
+        }
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        providerBrowseHistory = ProviderBrowseHistory(requireContext())
+    @Composable
+    private fun ToolBar(pagerState: PagerState) {
+        Column {
+            TopAppBar(
+                backgroundColor = MaterialTheme.colors.secondary,
+                contentPadding = rememberInsetsPaddingValues(LocalWindowInsets.current.statusBars),
+                title = { Text(viewModel.provider.title) },
+                actions = {
+                    IconButton(onClick = {
+//            queryHint = getString(R.string.menu_search_hint)
+//                    findNavController().navigate(
+//                        R.id.action_to_search,
+//                        bundleOf(
+//                            "provider" to viewModel.provider,
+//                            "keywords" to query
+//                        )
+//                    )
+                    }) {
+                        Icon(Icons.Filled.Search, contentDescription = "search")
+                    }
+                    MangaDisplayModeButton()
+                }
+            )
 
-        setupMenu(binding.toolbar.menu)
-        binding.toolbar.setOnMenuItemClickListener(this::onMenuItemSelected)
-        binding.toolbar.title = viewModel.provider.title
-        binding.toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
-        binding.toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
-
-        binding.viewPager.offscreenPageLimit = 3
-        binding.viewPager.adapter = ProviderPagerAdapter(
-            requireContext(),
-            childFragmentManager,
-            requireArguments()
-        )
-        binding.viewPager.currentItem =
-            providerBrowseHistory.getPageHistory(viewModel.provider.id).coerceIn(0, 2)
-        binding.tabs.setupWithViewPager(binding.viewPager)
+            TabRow(
+                modifier = Modifier.zIndex(2f),
+                selectedTabIndex = pagerState.currentPage,
+                backgroundColor = MaterialTheme.colors.secondary
+            ) {
+                listOf("Popular", "Latest", "Category").forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(title) },
+                        selected = pagerState.currentPage == index,
+                        onClick = { },
+                    )
+                }
+            }
+        }
     }
 
-    override fun onPause() {
-        super.onPause()
-        providerBrowseHistory.setPageHistory(viewModel.provider.id, binding.viewPager.currentItem)
+    @Composable
+    private fun Content(pagerState: PagerState) {
+        HorizontalPager(state = pagerState) { page ->
+            when (page) {
+                0 -> ProviderPanel(0, viewModel.popularMangaList, viewModel.popularOptionModel)
+                1 -> ProviderPanel(1, viewModel.latestMangaList, viewModel.latestOptionModel)
+                2 -> ProviderPanel(2, viewModel.categoryMangaList, viewModel.categoryOptionModel)
+            }
+        }
     }
 
-    private fun setupMenu(menu: Menu) {
-        with(menu.findItem(R.id.action_search).actionView as SearchView) {
-            queryHint = getString(R.string.menu_search_hint)
-            maxWidth = Int.MAX_VALUE
-            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String): Boolean {
-                    isIconified = true
-                    isIconified = true
+    @OptIn(ExperimentalMaterialApi::class)
+    @Composable
+    private fun ProviderPanel(
+        page: Int,
+        mangaList: ProviderMangaList,
+        optionModelLiveData: LiveData<OptionModel>
+    ) {
+        val optionModel by optionModelLiveData.observeAsState(mapOf())
+        Column {
+            OptionGroupList(
+                page = page,
+                mangaList = mangaList,
+                optionModel = optionModel
+            )
+
+            RefreshableMangaList(
+                mangaList = mangaList.list.collectAsLazyPagingItems(),
+                onCardClick = {
                     findNavController().navigate(
-                        R.id.action_to_search,
+                        R.id.action_to_gallery_detail,
                         bundleOf(
-                            "provider" to viewModel.provider,
-                            "keywords" to query
+                            "outline" to it,
+                            "provider" to viewModel.provider
                         )
                     )
-                    return true
+                },
+                onCardLongClick = {
+                    ProviderActionSheet(
+                        requireContext(),
+                        it,
+                        viewModel.provider.id,
+                        actionAdapter
+                    ).show()
                 }
+            )
+        }
+    }
 
-                override fun onQueryTextChange(query: String): Boolean = true
-            })
-            setOnQueryTextFocusChangeListener { _, b ->
-                if (!b && query.isNullOrBlank()) {
-                    isIconified = true
-                    isIconified = true
+    @Composable
+    private fun OptionGroupList(
+        page: Int,
+        mangaList: ProviderMangaList,
+        optionModel: OptionModel
+    ) {
+        val option: Option = mutableMapOf()
+        optionModel.map { (name, options) ->
+            val selectedIndex = providerBrowseHistory.getOptionHistory(
+                viewModel.provider.id, page, name
+            )
+            option[name] = selectedIndex
+            FlowRow(
+                modifier = Modifier.padding(bottom = 8.dp),
+                mainAxisSpacing = 4.dp,
+                crossAxisSpacing = 4.dp
+            ) {
+                options.mapIndexed { index, option ->
+                    Text(
+                        modifier = Modifier.clickable {
+                            providerBrowseHistory.setOptionHistory(
+                                viewModel.provider.id,
+                                page, name, index
+                            )
+                            mangaList.selectOption(name, index)
+                        },
+                        style = TextStyle(fontSize = 12.sp).merge(),
+                        text = option,
+                        color = if (index != selectedIndex) MaterialTheme.colors.onSurface
+                        else MaterialTheme.colors.primary
+                    )
                 }
             }
         }
-
-        menu.findItem(R.id.action_display_mode).setIcon(getDisplayModeIcon())
-    }
-
-    private fun onMenuItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_display_mode -> {
-                GlobalPreference.displayMode.setNext()
-                item.setIcon(getDisplayModeIcon())
-                true
-            }
-            else -> false
-        }
-    }
-}
-
-fun getDisplayModeIcon(): Int {
-    return when (GlobalPreference.displayMode.get()) {
-        GlobalPreference.DisplayMode.GRID -> R.drawable.ic_baseline_view_module_24
-        GlobalPreference.DisplayMode.LINEAR -> R.drawable.ic_baseline_view_list_24
+        if (optionModel.isNotEmpty()) mangaList.selectOption(option)
     }
 }
