4a00622ce2511a5a9b25d2d57f4de0bd9c79ae9c
FishHawk <whang177@gmail.com>
2021-07-16 16:50:26 +0800

    Refactor global search

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/globalsearch/GlobalSearchScreen.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/globalsearch/GlobalSearchScreen.kt
@@ -12,8 +12,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.NavigateNext
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -53,7 +53,8 @@ private fun ToolBar(navController: NavHostController) {
 @Composable
 private fun Content(navController: NavHostController) {
     val viewModel = hiltViewModel<GlobalSearchViewModel>()
-    val searchGroupList by viewModel.searchGroupList.observeAsState(listOf())
+    val searchGroupList by viewModel.searchGroupList.collectAsState()
+
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp),
@@ -61,20 +62,22 @@ private fun Content(navController: NavHostController) {
     ) {
         items(searchGroupList) {
             Column {
+                val searchGroup by it.collectAsState()
+
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text(it.provider.title)
+                    Text(searchGroup.provider.title)
                     Spacer(modifier = Modifier.weight(1f, fill = true))
                     IconButton(onClick = {
                         navController.currentBackStackEntry?.arguments =
                             bundleOf(
                                 "keywords" to viewModel.keywords.value,
-                                "provider" to it.provider
+                                "provider" to searchGroup.provider
                             )
-                        navController.navigate("search/${it.provider.id}")
+                        navController.navigate("search/${searchGroup.provider.id}")
                     }) { Icon(Icons.Filled.NavigateNext, "Forward") }
                 }
-                val provider = it.provider
-                it.result?.getOrNull()?.let {
+
+                searchGroup.result?.getOrNull()?.let {
                     LazyRow(
                         modifier = Modifier.height(140.dp),
                         horizontalArrangement = Arrangement.spacedBy(8.dp),
@@ -88,7 +91,7 @@ private fun Content(navController: NavHostController) {
                                     navController.currentBackStackEntry?.arguments =
                                         bundleOf(
                                             "outline" to it,
-                                            "provider" to provider
+                                            "provider" to searchGroup.provider
                                         )
                                     navController.navigate("gallery/${it.id}")
                                 })
@@ -96,7 +99,7 @@ private fun Content(navController: NavHostController) {
                         }
                     }
                 }
-                it.result?.exceptionOrNull()?.let {
+                searchGroup.result?.exceptionOrNull()?.let {
                     ErrorItem(message = it.message ?: "", onClickRetry = {})
                 }
             }
--- app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/globalsearch/GlobalSearchViewModel.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/globalsearch/GlobalSearchViewModel.kt
@@ -6,7 +6,7 @@ import com.fishhawk.driftinglibraryandroid.data.remote.ResultX
 import com.fishhawk.driftinglibraryandroid.data.remote.model.MangaOutline
 import com.fishhawk.driftinglibraryandroid.data.remote.model.ProviderInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 data class SearchGroup(
@@ -20,30 +20,17 @@ class GlobalSearchViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    val keywords = MutableLiveData(savedStateHandle.get<String>("keywords"))
+    val keywords = MutableStateFlow(savedStateHandle.get<String>("keywords") ?: "")
 
-    private val providerList = liveData {
-        emit(null)
-        emit(remoteLibraryRepository.listProvider())
-    }
+    private val providerList =
+        flow { emit(remoteLibraryRepository.listProvider().getOrNull()) }.filterNotNull()
 
-    val searchGroupList: MediatorLiveData<List<SearchGroup>> = MediatorLiveData()
-
-    init {
-        searchGroupList.addSource(keywords) { search() }
-        searchGroupList.addSource(providerList) { search() }
-    }
-
-    private fun search() =
-        viewModelScope.launch {
-            val keywords = keywords.value ?: return@launch
-            val it = providerList.value?.getOrNull() ?: return@launch
-
-            searchGroupList.value = it.map { info ->
-                SearchGroup(info, null)
-            }
-            searchGroupList.value = it.map { info ->
-                SearchGroup(info, remoteLibraryRepository.listManga(info.id, keywords, 1))
-            }
+    val searchGroupList = combine(keywords, providerList) { keywords, providerList ->
+        providerList.map { provider ->
+            flow {
+                val result = remoteLibraryRepository.listManga(provider.id, keywords, 1)
+                emit(SearchGroup(provider, result))
+            }.stateIn(viewModelScope, SharingStarted.Lazily, SearchGroup(provider, null))
         }
+    }.stateIn(viewModelScope, SharingStarted.Lazily, listOf())
 }
