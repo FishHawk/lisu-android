b5bb0246096513afa9d0cf466c2935e40e937b58
FishHawk <whang177@gmail.com>
2021-07-24 22:26:51 +0800

    Clean code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,8 @@ android {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
 
+    implementation("androidx.activity:activity-ktx:1.3.0-rc02")
+
     implementation("androidx.compose.ui:ui:1.0.0-rc02")
     implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
     implementation("androidx.compose.foundation:foundation:1.0.0-rc02")
@@ -91,6 +93,10 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core:1.0.0-rc02")
     implementation("androidx.compose.material:material-icons-extended:1.0.0-rc02")
     implementation("androidx.compose.runtime:runtime-livedata:1.0.0-rc02")
+
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0-rc01")
 
@@ -100,6 +106,7 @@ dependencies {
 
     implementation("androidx.paging:paging-runtime-ktx:3.0.0")
     implementation("androidx.paging:paging-compose:1.0.0-alpha11")
+
 
     implementation("com.google.accompanist:accompanist-flowlayout:0.14.0")
     implementation("com.google.accompanist:accompanist-insets:0.14.0")
@@ -113,15 +120,16 @@ dependencies {
     implementation("io.coil-kt:coil:1.3.0")
     implementation("io.coil-kt:coil-compose:1.3.0")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+
+
     implementation("com.google.dagger:hilt-android:2.37")
     kapt("com.google.dagger:hilt-android-compiler:2.37")
     implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
-
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
 
     // UI Tests
     testImplementation("junit:junit:4.13.2")
@@ -130,25 +138,9 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0-rc02")
 
 
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("androidx.core:core-ktx:1.6.0")
-
-    implementation("androidx.activity:activity-ktx:1.3.0-rc02")
-
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    // Wait to remove
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-
-    // Restful
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
-    implementation("com.google.code.gson:gson:2.8.7")
-
-    // UI library
     implementation("com.google.android.material:material:1.5.0-alpha01")
-
-    // OpenCV
     implementation("com.quickbirdstudios:opencv:4.1.0")
 }
