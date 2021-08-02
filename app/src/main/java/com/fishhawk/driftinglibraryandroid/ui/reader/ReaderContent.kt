d39eb3eefbfb10276eed58eed14bf8dfa62c707a
FishHawk <whang177@gmail.com>
2021-08-02 15:56:07 +0800

    Clean code

--- app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/reader/ReaderContent.kt
+++ app/src/main/java/com/fishhawk/driftinglibraryandroid/ui/reader/ReaderContent.kt
@@ -1,9 +1,18 @@
 package com.fishhawk.driftinglibraryandroid.ui.reader
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -13,15 +22,29 @@ import androidx.compose.ui.input.key.*
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.Velocity
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.fishhawk.driftinglibraryandroid.PR
 import com.fishhawk.driftinglibraryandroid.data.datastore.ReaderMode
 import com.fishhawk.driftinglibraryandroid.data.datastore.collectAsState
+import com.fishhawk.driftinglibraryandroid.util.interceptor.ProgressInterceptor
 import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.VerticalPager
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.launch
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
 //        reader.onPageLongClicked = { position, url ->
 //            if (P.isLongTapDialogEnabled.get())
@@ -168,15 +191,127 @@ fun ReaderContent(
             true
         }
     ) {
-        PagerReader(
-            pagerState,
-            pointer.currChapter.images,
-            onTap = { viewModel.isMenuOpened.value = !viewModel.isMenuOpened.value }
-        )
+        val onTap = { offset: Offset ->
+            if (viewModel.isMenuOpened.value) viewModel.isMenuOpened.value = false
+            else when {
+                offset.x < 0.25 -> toLeft()
+                offset.x > 0.75 -> toRight()
+                else -> viewModel.isMenuOpened.value = !viewModel.isMenuOpened.value
+            }
+        }
+
+        val isPageIntervalEnabled by PR.isPageIntervalEnabled.collectAsState()
+        val itemSpacing = if (isPageIntervalEnabled) 16.dp else 0.dp
+
+        val layoutDirection =
+            if (readerDirection == ReaderMode.Rtl) LayoutDirection.Rtl
+            else LayoutDirection.Ltr
+
+        CompositionLocalProvider(LocalLayoutDirection provides layoutDirection) {
+            if (readerDirection == ReaderMode.Vertical) {
+                VerticalPager(state = pagerState, itemSpacing = itemSpacing) { index ->
+                    Page(
+                        position = index.plus(1),
+                        url = pointer.currChapter.images[index],
+                        onTap = onTap
+                    )
+                }
+            } else {
+                HorizontalPager(state = pagerState, itemSpacing = itemSpacing) { index ->
+                    Page(
+                        position = index.plus(1),
+                        url = pointer.currChapter.images[index],
+                        onTap = onTap
+                    )
+                }
+            }
+        }
     }
 
     DisposableEffect(Unit) {
         focusRequester.requestFocus()
         onDispose { }
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun Page(
+    position: Int,
+    url: String,
+    onTap: ((Offset) -> Unit)? = null
+) {
+    val painter = rememberImagePainter(url)
+    var layout: LayoutCoordinates? = null
+    Box(
+        modifier = Modifier
+            .onGloballyPositioned { layout = it }
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onPress = { /* Called when the gesture starts */ },
+                    onDoubleTap = { /* Called on Double Tap */ },
+                    onLongPress = { /* Called on Long Press */ },
+                    onTap = { offset ->
+                        layout?.let {
+                            onTap?.invoke(
+                                Offset(
+                                    x = offset.x / it.parentCoordinates!!.size.width,
+                                    y = offset.y / it.parentCoordinates!!.size.height
+                                )
+                            )
+                        }
+                    }
+                )
+            }
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Fit
+        )
+        when (val state = painter.state) {
+            is ImagePainter.State.Loading -> {
+                var progress by remember { mutableStateOf<Float?>(null) }
+                Column(
+                    modifier = Modifier.align(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(
+                        text = position.toString(),
+                        style = MaterialTheme.typography.h3
+                    )
+                    progress?.let { CircularProgressIndicator(progress = it) }
+                        ?: CircularProgressIndicator()
+                }
+                DisposableEffect(state) {
+                    val key = url.toHttpUrlOrNull().toString()
+                    ProgressInterceptor.addListener(key) { progress = it }
+                    onDispose {
+                        ProgressInterceptor.removeListener(key)
+                        progress = null
+                    }
+                }
+            }
+            is ImagePainter.State.Error -> {
+                Column(
+                    modifier = Modifier.align(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(
+                        text = position.toString(),
+                        style = MaterialTheme.typography.h3
+                    )
+                    Text(
+                        text = state.throwable.message ?: "",
+                        style = MaterialTheme.typography.body1
+                    )
+                    TextButton(onClick = { }) { Text("retry") }
+                }
+            }
+            else -> Unit
+        }
     }
 }
